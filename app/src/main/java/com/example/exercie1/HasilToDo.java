36aaf8b15a1f666600168b1bc46e5491162e95f1
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-26 19:37:19 +0700

    Deklarasi variable TextView pada HasilToDo dan deklarasi variable EditText,FAB, serta variable yang berguna untuk menyimpan data dari edittext pada InputTodo

--- app/src/main/java/com/example/exercie1/HasilToDo.java
+++ app/src/main/java/com/example/exercie1/HasilToDo.java
@@ -3,12 +3,28 @@ package com.example.exercie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HasilToDo extends AppCompatActivity {
+
+    /* Deklarasai TextView pada Hasil ToDo*/
+    TextView txTask, txJTask, txTTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_to_do);
+
+        //Menghubungkan setiap variable dengan component yang sudah ditentukan pada layout acitivty_hasil_to_do.xml
+        txTask = findViewById(R.id.tvHslEdTask);
+        txJTask = findViewById(R.id.tvJHslEdTask);
+        txTTask = findViewById(R.id.tvTHslEdTask);
+
+        //Membuat objek bundle dan mengambil data yang dikirimkan dari activity InputTodo
+        Bundle bundle = getIntent().getExtras();
+
+
+
     }
 }
--- app/src/main/java/com/example/exercie1/InputTodo.java
+++ app/src/main/java/com/example/exercie1/InputTodo.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class InputTodo extends AppCompatActivity {
 
     /* Deklarasai TextView pada Input ToDo */
     TextView txUserLgn, txUserRgs;
+
+    //Mendeklarasi variable Edit text dan FloatingActionButton
+    EditText edTask,edJTask,edTTask;
+    FloatingActionButton fabAdd;
+
+    //Mendeklarasi variable untuk menyimpan data dari edittext pada layout
+    String sTask,sJTask,sTTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,7 @@ public class InputTodo extends AppCompatActivity {
 
         //Menghubungkan variable text view dengan component TextView pada activity_input_todo
         txUserRgs = findViewById(R.id.lInputRgs);
+
         //Mendeklarasikan variable bundle untuk mengambil pesan yang dikirimkan dengan method intent
         Bundle bundle = getIntent().getExtras();
 
@@ -30,9 +41,9 @@ public class InputTodo extends AppCompatActivity {
         //Membuat variable untuk menampung data dari activity Sign In
         String userRgs = bundle.getString("user2");
 
+        //berguna untuk mengubah textview menjadi data yang berasal dari SignIn dan Register
         txUserLgn.setText(userSgn);
         txUserRgs.setText(userRgs);
-
 
 
 
