2a36e9f1fa434194c6d885223dcecdf1ac662925
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-26 19:08:12 +0700

    Menambahkan Activity dan layout baru pada project serta menambahkan parent home pada AndroidManifest.xml

---
+++ app/src/main/java/com/example/exercie1/HasilToDo.java
@@ -0,0 +1,14 @@
+package com.example.exercie1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_to_do);
+    }
+}
