824232e6cbe25600c070c94dbc73077356336cce
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-26 16:52:24 +0700

    Deklarasi EditText,FloatingActionButton dan menghubungkan variable dengan component pada activity_register.xml

--- app/src/main/java/com/example/exercie1/Register.java
+++ app/src/main/java/com/example/exercie1/Register.java
@@ -3,16 +3,35 @@ package com.example.exercie1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class Register extends AppCompatActivity {
 
-
+    // Deklarasi variabel dengan tipe data Edit Text dan FloatingAction Button
+    EditText edRgUser, edRgEmail, edRgPass, edRgRepass;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edRgUser = findViewById(R.id.edtxtNamaRgs);
+        edRgEmail = findViewById(R.id.edtxtEmailRgs);
+        edRgPass = findViewById(R.id.edtxtPassRgs);
+        edRgRepass = findViewById(R.id.edtxtRePassRgs);
+        fab = findViewById(R.id.fabTambah);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
