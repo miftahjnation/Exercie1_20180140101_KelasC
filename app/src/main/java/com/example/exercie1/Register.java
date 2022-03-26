ae6d5937905233d43b068f847057fa8b9520ba83
miftahjnation <62986167+miftahjnation@users.noreply.github.com>
2022-03-26 19:01:41 +0700

    Menambahkan statement pada Register.java agar dapat memunculkan notifikasi Error jika 1 EditText saja yang diisi atau password dan repassword aja yang diisi

--- app/src/main/java/com/example/exercie1/Register.java
+++ app/src/main/java/com/example/exercie1/Register.java
@@ -50,19 +50,69 @@ public class Register extends AppCompatActivity {
                     edRgPass.setError("Password wajib diisi");
                     edRgRepass.setError("Re-password wajib diisi");
                 }
+                //Statement jika hanya user yang diisi.
+                else if(passRgs.isEmpty() && rePassRgs.isEmpty() && emailRgs.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Silahkan isi Email, Password dan Repassword", Toast.LENGTH_LONG).show();
+                    edRgEmail.setError("Email wajib diisi");
+                    edRgPass.setError("Password wajib diisi");
+                    edRgRepass.setError("Re-password wajib diisi");
+                }
+                //Statement jika hanya email yang diisi.
+                else if(userRgs1.isEmpty() && passRgs.isEmpty() && rePassRgs.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Silahkan isi User, Password dan Repassword", Toast.LENGTH_LONG).show();
+                    edRgUser.setError("User wajib diisi");
+                    edRgPass.setError("Password wajib diisi");
+                    edRgRepass.setError("Re-password wajib diisi");
+                }
+                //Statement jika hanya password yang diisi.
+                else if(userRgs1.isEmpty() && rePassRgs.isEmpty() && emailRgs.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Silahkan isi User, Email dan Repassword", Toast.LENGTH_LONG).show();
+                    edRgUser.setError("User wajib diisi");
+                    edRgEmail.setError("Email wajib diisi");
+                    edRgRepass.setError("Re-password wajib diisi");
+                }
+                //Statement jika hanya repass yang diisi.
+                else if(userRgs1.isEmpty() && emailRgs.isEmpty()&& passRgs.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Silahkan isi User, Email dan Password", Toast.LENGTH_LONG).show();
+                    edRgUser.setError("User wajib diisi");
+                    edRgEmail.setError("Email wajib diisi");
+                    edRgPass.setError("Password wajib diisi");
+                }
+                //Statement jika pass dan repass dikosongkan.
                 else if(passRgs.isEmpty() && rePassRgs.isEmpty())
                 {
-                    //Menampilkan pesan notifikasi jika password kosong
                     Toast.makeText(getApplicationContext(), "Silahkan isi Password dan Repassword", Toast.LENGTH_LONG).show();
+                    edRgPass.setError("Password wajib diisi");
+                    edRgRepass.setError("Re-password wajib diisi");
                 }
                 else
                 {
                     //Membuat kondisi untuk mengecek apakah isi dari EditText password dan EditText repassword
                     //sama atau tidak
-                    if(passRgs.equals(rePassRgs))
+                    if(passRgs.equals(rePassRgs) && emailRgs.isEmpty() && userRgs1.isEmpty())
                     {
-
-
+                        //Menampilkan pesan notifikasi jika user dan email kosong
+                        Toast.makeText(getApplicationContext(), "Silahkan isi user dan email! jangan dikosongkan.", Toast.LENGTH_LONG).show();
+                        edRgUser.setError("User wajib diisi");
+                        edRgEmail.setError("Email wajib diisi");
+                    }
+                    else if(passRgs.equals(rePassRgs) && userRgs1.isEmpty() )
+                    {
+                        //Menampilkan pesan notifikasi jika user kosong
+                        Toast.makeText(getApplicationContext(), "Silahkan isi user! jangan dikosongkan.", Toast.LENGTH_LONG).show();
+                        edRgUser.setError("User wajib diisi");
+                    }
+                    else if(passRgs.equals(rePassRgs) && emailRgs.isEmpty() )
+                    {
+                        //Menampilkan pesan notifikasi jika email kosong
+                        Toast.makeText(getApplicationContext(), "Silahkan isi email! jangan dikosongkan.", Toast.LENGTH_LONG).show();
+                        edRgEmail.setError("Email wajib diisi");
+                    }
+                    else if(passRgs.equals(rePassRgs) )
+                    {
                         //Menampilkan pesan notifikasi jika pendaftaran berhasil
                         Toast.makeText(getApplicationContext(), "Pendaftaran Berhasil...", Toast.LENGTH_LONG).show();
 
